129ae56453305d1b3b53aa63d08f2c2b20525b68
Kyle Bogart <38845982+CITstudent2019@users.noreply.github.com>
2019-02-05 21:45:10 -0600

    Add files via upload

---
+++ api_call.py
@@ -0,0 +1,32 @@
+# author: kyle bogart
+# desc: utilizes github api to receive python repository data
+# 02/05/2019
+
+import requests 
+
+keepGoing = 'y'
+
+while (keepGoing == "y"):
+	lang = raw_input("Language to Query: ")
+
+	url = 'https://api.github.com/search/repositories?q=language:' + lang + '&sort=stars'
+	r = requests.get(url)
+
+	print("Status code: " + str(r.status_code))
+
+	response_dict = r.json()
+	# keys are total_count, items, incomplete_results
+	print("Number of Repos: " + str(response_dict['total_count']))
+	print("------------------------------------------------------------------")
+
+	repo_dicts = response_dict['items']
+	print("Top Repositories: ")
+	
+	count = 1
+	for repo_dict in repo_dicts:
+		print(str(count) + ". " + repo_dict['name'])
+		count += 1
+		
+	print("------------------------------------------------------------------")
+
+	
